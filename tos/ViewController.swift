870d521c2b1c1e8b906e93306fc331dfffc0da99
Karandeep <karan99130@gmail.com>
2015-05-05 13:34:11 -0400

    Initial Commit

---
+++ tos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tos
+//
+//  Created by Karandeep Karandeep on 2015-05-05.
+//  Copyright (c) 2015 Karandeep Karandeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
